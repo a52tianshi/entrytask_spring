03bb96d13981ac213942ef2117a464e7f5128a11
Penny Hardaway <475215536@qq.com>
2024-12-24 18:07:16 +0800

    add redis

--- src/main/java/com/cq/demo/HelloController.java
+++ src/main/java/com/cq/demo/HelloController.java
@@ -3,11 +3,19 @@ package com.cq.demo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import redis.clients.jedis.Jedis;
 
 @RestController
 public class HelloController {
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+
+        Jedis jedis = new Jedis("localhost");
+        System.out.println("连接成功");
+        //设置 redis 字符串数据
+        jedis.set("name", name);
+        jedis.close();
+
         return String.format("Hello %s!", name);
     }
 }
